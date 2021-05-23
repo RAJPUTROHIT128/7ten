efc0dfb6c76f6f9f22cec6460e723c12a266c3d0
rajputrohit128 <rajputrohit128@gmail.com>
2021-05-24 00:07:28 +0530

    third commit (creating first route)

---
+++ backend/route/product.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router();
+
+const { getProducts } = require("../controllers/productControllers")
+
+
+router.route("/products").get(getProducts);
+
+module.exports = router;
