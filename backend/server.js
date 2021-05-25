4f327fe8f3e408358b551414b3c97adaa4dc7cf3
rajputrohit128 <rajputrohit128@gmail.com>
2021-05-25 23:06:59 +0530

    fourth commit (configuring mongodb connection)

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
@@ -6,6 +7,9 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "backend/config/config.env"})
 
-app.listen(process.env.PORT, () =>{
+//connecting to Database
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
     console.log(`Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
 })
