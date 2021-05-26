9704ef368586965b1da4c2770a00b229c6312c96
rajputrohit128 <rajputrohit128@gmail.com>
2021-05-26 23:55:54 +0530

    Updating Product by it's ID value

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product")
 
-//Create new Product  Path(/api/v1/product/new)
+//Create new Product  Path(/api/v1/admin/product/new)
 
 
 exports.newProduct = async(req, res, next) =>{
@@ -30,3 +30,52 @@ exports.getProducts = async (req, res, next) =>{
     }
 }
 
+
+//getting single product description (/api/v1/product/:id)
+
+exports.getSingleProduct = async (req, res, next) =>{
+
+    try{
+        const product = await Product.findById(req.params.id);
+
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: 'Product not found'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            product
+        })
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
+
+//Updating Product (/api/v1/admin/product/:id)
+
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
--- backend/route/product.js
+++ backend/route/product.js
@@ -1,13 +1,21 @@
 const express = require("express")
 const router = express.Router();
 
-const { getProducts, newProduct } = require("../controllers/productControllers")
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require("../controllers/productControllers")
 
 //product showing page
 router.route("/products").get(getProducts);
 
+//single product description page
+
+router.route("/product/:id").get(getSingleProduct);
+
 
 //new product adding page
-router.route("/product/new").post(newProduct);
+router.route("/admin/product/new").post(newProduct);
+
+//Updating Product
+
+router.route("/admin/product/:id").put(updateProduct);
 
 module.exports = router;
