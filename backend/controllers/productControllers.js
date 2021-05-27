ae5ad58e854729de8b4e7c7b714b45a07db04591
rajputrohit128 <rajputrohit128@gmail.com>
2021-05-27 15:06:46 +0530

    catching Async error, handling UnHandled Promise Rejection and Handling Uncaught Exception

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,40 +2,34 @@ const Product = require("../models/product")
 
 const ErrorHandler = require("../utility/errorHandler");
 
+const catchAsyncError = require("../middlewares/catchAsyncError");
+
 //Create new Product  Path(/api/v1/admin/product/new)
 
 
-exports.newProduct = async(req, res, next) =>{
-    try{
+exports.newProduct = catchAsyncError(async(req, res, next) =>{
     const product = await Product.create(req.body);
     res.status(201).json({
         success: true,
         product
-    })}
-    catch(error){
-        console.log(error);
-    }
-}
+    })
+})
 
 //getting all products (/api/v1/products)
 
-exports.getProducts = async (req, res, next) =>{
-    try{
+exports.getProducts = catchAsyncError(async (req, res, next) =>{
         const products = await Product.find();
         res.status(200).json({
             success: true,
             count: products.length,
             products
         })
-    }catch(error){
-        console.log(error.message);
-    }
-}
+})
 
 
 //getting single product description (/api/v1/product/:id)
 
-exports.getSingleProduct = async (req, res, next) =>{
+exports.getSingleProduct = catchAsyncError(async (req, res, next) =>{
 
         const product = await Product.findById(req.params.id);
 
@@ -47,20 +41,16 @@ exports.getSingleProduct = async (req, res, next) =>{
             success: true,
             product
         })
-}
+})
 
 
 //Updating Product (/api/v1/admin/product/:id)
 
-exports.updateProduct = async (req, res, next) => {
-    try{
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
         let product = await Product.findById(req.params.id);
 
         if(!product){
-            return res.status(404).json({
-                success: false,
-                message: 'Product not found'
-            })
+            return next(new ErrorHandler("Product not found", 404));
         }
 
         product = await Product.findByIdAndUpdate(req.params.id, req.body,{
@@ -73,24 +63,17 @@ exports.updateProduct = async (req, res, next) => {
             success: true,
             product
         })
-
-    }catch(error){
-        console.log(error.message);
-    }
-}
+})
 
 
 //Deleting Product   (/api/v1/admin/product/:id)
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
 
     const product = await Product.findById(req.params.id);
 
         if(!product){
-            return res.status(404).json({
-                success: false,
-                message: 'Product not found'
-            })
+            return next(new ErrorHandler("Product not found", 404));
         }
 
         await product.remove();
@@ -99,4 +82,4 @@ exports.deleteProduct = async (req, res, next) => {
             success: true,
             message: 'Product is successfully removed'
         })
-}
+})
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,32 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
+//Handle Uncaught Exception
+process.on("uncaughtException",err =>{
+    console.log(`Error: ${err.stack}`);
+    console.log("Shutting down server due to uncaughtException");
+    process.exit(1);
+})
+
 //Setting up configuration file
 
 dotenv.config({ path: "backend/config/config.env"})
 
+
+
 //connecting to Database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+//Handle Unhandled Promise rejection
+
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Unhandled Promise Rejection");
+    server.close(() =>{
+        process.exit(1);
+    })
 })
