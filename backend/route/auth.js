3e864673c93f1526c445c04c72a10adb68543480
rajputrohit128 <rajputrohit128@gmail.com>
2021-06-01 15:43:49 +0530

    getting user details by id for admin

--- backend/route/auth.js
+++ backend/route/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile, updatePassword, updateProfile} = require("../controllers/userControllers");
+const {
+    registerUser,
+    loginUser, 
+    logoutUser, 
+    forgotPassword, 
+    resetPassword, 
+    getUserProfile, 
+    updatePassword, 
+    updateProfile,
+    getAllUsers,
+    getUserDetails} = require("../controllers/userControllers");
 
-const {isAuthenticatedUser} = require("../middlewares/auth");
+const {isAuthenticatedUser, authorizeRoles} = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
@@ -13,4 +23,10 @@ router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/logout").get(logoutUser);
+
+
+//admin routes pages
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails);
+
 module.exports = router;
